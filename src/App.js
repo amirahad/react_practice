4040190784af88b6db9217fd6d6e2d16fbab5a60
amirahad <arahad4783@gmail.com>
2021-09-15 16:04:38 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
 import React, {Component} from 'react';
-import './App.css';
-import Person from './Person/Person'
-import UserInput from './UserInput/UserInput';
-import UserOutput from './UserOutput/UserOutput';
+import './App.css'; 
+import Counter from './components/counter';
 
-class App extends Component {
-  state = {
-    username: 'Hey Bro!'
-  }
 
-  inputChangedHandeler = (event) => {
-    this.setState({username: event.target.value})
-  }
+class App extends Component { 
 
 
   render(){
     return (
       <div className="App"> 
-          <h1>Hey! I am learning REACT</h1> 
-          <h2>Myself, Amir Ahad</h2>
-          {/* <Person name="Amir" age="22"/>
-          <Person name="Raza" age="21"/>
-          <Person name="Ahad" age="25"/>  */}
-          <UserOutput userName={this.state.username} />
-          <UserOutput userName="Raza" />
-          <UserOutput userName={this.state.username} />
-          <UserInput changed={this.inputChangedHandeler} />
+        <Counter />
       </div>
     );
   };
---
+++ src/components/counter.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+
+class counter extends Component{
+    state= {
+        count: 0 
+    };
+
+    render(){
+        return(
+            <div>
+                <h1>{this.formatCount()}</h1>
+                <button>Increment</button>
+            </div>
+        );
+    }
+    formatCount(){
+        const {count} = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
+}
+
+
+export default counter;
