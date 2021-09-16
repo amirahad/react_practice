a77fdfad529d8670f6ae36aa8d8a54cb34243fc1
amirahad <arahad4783@gmail.com>
2021-09-17 02:27:28 +0600

    modified

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -55,22 +55,4 @@ class Movies extends Component {
 }
 
 export default Movies;
-
-<table className="table table-dark">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-  </tbody>
-</table>;
+ 
